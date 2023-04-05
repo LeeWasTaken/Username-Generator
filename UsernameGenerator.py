074e2ebb77ae2897f8dbfcb37df516eda88079b1
LeeWasTaken <129436032+LeeWasTaken@users.noreply.github.com>
2023-04-06 08:21:29 +0900

    Update UsernameGenerator.py

--- UsernameGenerator.py
+++ UsernameGenerator.py
@@ -2,16 +2,15 @@ import requests
 import json
 import string
 import random
-NOU = int(input('Number Of Usernames You Want To Generate : '))
-NOL = int(input('Number Of Letters You Want : '))
-def Generate():
-    username = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(NOL))
-    api = f"https://auth.roblox.com/v1/usernames/validate?request.username={username}&request.birthday=1982-08-08"
-    r = requests.get(api).json()
-    if r ['code'] != 0:
-        print(username, '(Invalid!)')
-    else:
-        print(username, '(Valid!)')
-
-for i in range(NOU):
-    Generate()
+NOU =int (input ('Number Of Usernames You Want To Generate : '))
+NOL =int (input ('Number Of Letters You Want : '))
+def Generate ():
+    O0O000OOOO0OOO000 =''.join (random .choice (string .ascii_letters +string .digits )for O00O0O00OOO0000OO in range (NOL ))
+    OO00O0OOO00OO000O =f"https://auth.roblox.com/v1/usernames/validate?request.username={O0O000OOOO0OOO000}&request.birthday=1982-08-08"
+    O00000OOO00OOOOOO =requests .get (OO00O0OOO00OO000O ).json ()
+    if O00000OOO00OOOOOO ['code']!=0 :
+        print (O0O000OOOO0OOO000 ,'(Invalid!)')
+    else :
+        print (O0O000OOOO0OOO000 ,'(Valid!)')
+for i in range (NOU ):
+    Generate ()
