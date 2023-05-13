eb6bcfc3a98bfdf86ab94799137686cd44c7931c
LeeWasTaken <129436032+LeeWasTaken@users.noreply.github.com>
2023-05-13 21:53:14 +0100

    Update UsernameGenerator.py

--- UsernameGenerator.py
+++ UsernameGenerator.py
@@ -2,6 +2,19 @@ import requests
 import json
 import string
 import random
+import os
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+
+colorama_init()
+
+print(f"{Fore.GREEN}------------------{Style.RESET_ALL}")
+print(f"{Fore.GREEN}Username Generator{Style.RESET_ALL}")
+print(f"{Fore.GREEN}------------------{Style.RESET_ALL}")
+
+os.system('title Username Generator')
+
 NOU =int (input ('Number Of Usernames You Want To Generate : '))
 NOL =int (input ('Number Of Letters You Want : '))
 def Generate ():
@@ -9,8 +22,8 @@ def Generate ():
     OO00O0OOO00OO000O =f"https://auth.roblox.com/v1/usernames/validate?request.username={O0O000OOOO0OOO000}&request.birthday=1982-08-08"
     O00000OOO00OOOOOO =requests .get (OO00O0OOO00OO000O ).json ()
     if O00000OOO00OOOOOO ['code']!=0 :
-        print (O0O000OOOO0OOO000 ,'(Invalid!)')
+        print (O0O000OOOO0OOO000 ,'(Invalid Username!)')
     else :
-        print (O0O000OOOO0OOO000 ,'(Valid!)')
+        print (O0O000OOOO0OOO000 ,'(Valid Username!)')
 for i in range (NOU ):
     Generate ()
